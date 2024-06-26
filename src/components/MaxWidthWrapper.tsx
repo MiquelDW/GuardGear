6f35364e3c6c9d0d28847c08a68da2a5813af25a
miquel <miquel.dewit@hotmail.com>
2024-06-26 16:18:40 +0200

    Complete First Section

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -1,3 +1,23 @@
-export default function MaxWidthWrapper() {
-  return <div></div>;
+// use 'cn' helper function to merge the default classNames with the given classNames send as a prop
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+// reusable wrapper component that creates consistent padding, width etc on elements across the application
+export default function MaxWidthWrapper({
+  className,
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) {
+  return (
+    <div
+      className={cn(
+        "mx-auto h-full w-full max-w-screen-xl px-2.5 md:px-20",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
 }
---
+++ src/components/Phone.tsx
@@ -0,0 +1,60 @@
+// use 'cn' helper function to merge the default classNames with the given classNames send as a prop
+import { cn } from "@/lib/utils";
+import { HTMLAttributes } from "react";
+
+// predefine object structure for the given 'props' object
+// inherit all standard 'div' attributes like 'id', 'className', etc
+// using 'interface' instead of 'type' in this context is often preferred because interface allows for declaration merging, which means you can extend or merge additional properties into the same interface later. This is useful for building more complex and extensible types in React components. However, both interface and type can often be used interchangeably in simpler cases.
+// https://stackoverflow.com/questions/37233735/interfaces-vs-types-in-typescript
+interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
+  imgSrc: string;
+  dark?: boolean;
+}
+
+// you can also extend 'type' using intersection types.
+// Example: this combines properties from 'HTMLAttributes<HTMLDivElement>' with the given custom props to 'PhoneProps'
+// type PhoneProps = HTMLAttributes<HTMLDivElement> & {
+//   imgSrc: string;
+//   dark?: boolean;
+// };
+
+// reusable component that displays a phonecase with a given phonecase-img
+export default function Phone({
+  imgSrc,
+  className,
+  dark = false,
+  // collect all the other given props with the rest operator
+  ...props
+}: PhoneProps) {
+  return (
+    <div
+      className={cn(
+        // UNDERSTAND THE Z-INDEX STRUCTURE
+        "pointer-events-none relative z-50 overflow-hidden",
+        className,
+      )}
+      // spread out all the other given props in the <div> element
+      {...props}
+    >
+      {/* phone case (z-50: shows on top of given phonecase-img) */}
+      <img
+        src={
+          dark
+            ? "/phone-template-dark-edges.png"
+            : "/phone-template-white-edges.png"
+        }
+        className="pointer-events-none z-50 select-none"
+        alt="phone image"
+      />
+
+      {/* image inside the phone case (-z-10: shows below phonecase) */}
+      <div className="absolute inset-0 -z-10">
+        <img
+          src={imgSrc}
+          className="min-h-full min-w-full object-cover"
+          alt="overlaying phone image"
+        />
+      </div>
+    </div>
+  );
+}
